0a0d029fbf3e1069bae2f73f365309fc202a3a24
Pasupuleti Niranjan <pasupuletiniranjan@example.com>
2025-10-19 12:51:06 +0530

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar.jsx";
+import HeroSection from "./components/HeroSection.jsx";
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
         <main>
             <Navbar />
+            <HeroSection />
         </main>
     </>
   )
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,28 @@
+import {useEffect, useRef} from "react";
 
 let HeroSection = () => {
+    let videoRef = useRef();
+
+    useEffect(() => {
+        if(videoRef.current) {
+            videoRef.current.playbackRate = 2;
+        }
+    }, []);
+
     return (
         <>
+            <section id="hero">
+                <div>
+                    <h1>MacBook Pro</h1>
+                    <img src="/title.png" alt="Macbook Title" />
+                </div>
 
+               <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline/>
+
+                <button>Buy</button>
+
+                <p>From $1599 or $133/mo for 12 months</p>
+            </section>
         </>
     )
 }
